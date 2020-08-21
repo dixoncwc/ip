82b09a65ef99abc2df5b3b12983710ae30ee857d
LAPTOP-QPAQOSSJ\dixon <dixon.1996@hotmail.co.uk>
2020-08-21 18:12:37 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,7 @@
 public class Duke {
+
+    private static final String boundary = "\n____________________________________________________________\n";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +9,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(boundary+"Hello! I'm Duke\n" + "What can I do for you?\n" +boundary);
+        System.out.println("Bye. Hope to see you again soon");
+
     }
 }
