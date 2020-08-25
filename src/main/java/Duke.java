fdc80d49485a1ef59809b2963d4e87c651eb8169
LAPTOP-QPAQOSSJ\dixon <dixon.1996@hotmail.co.uk>
2020-08-25 20:54:24 +0800

    Level1-Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
 
-    private static final String boundary = "\n____________________________________________________________\n";
+    private static final String boundary = "____________________________________________________________";
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        boolean endconvo = false;
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("\nHello from\n" + logo);
 
-        System.out.println(boundary+"Hello! I'm Duke\n" + "What can I do for you?\n" +boundary);
+        System.out.println(boundary+"\nHello! I'm Duke\n" + "What can I do for you?\n" +boundary);
         System.out.println("Bye. Hope to see you again soon");
+
+
+        while(!endconvo){
+            String txt = scan.nextLine();
+
+            if(txt.equals("bye")){
+                endconvo = true;
+            }
+
+            else{
+                endconvo = false;
+            }
+
+            System.out.println(boundary+"\n" + txt + "\n"  +boundary);
+
+        }
+
+        System.out.println(boundary+ "\nBye. Hope to see you again soon\n" +boundary);
+
+
 
     }
 }
